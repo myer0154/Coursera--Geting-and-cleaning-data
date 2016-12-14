b0773a6e65d61b78dca593b014f855ec65750321
Jason Myers <myer0154@umn.edu>
2016-12-14 14:25:57 -0600

    Extract std/mean columns, add activity labels

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,10 @@
-## If the data files don't already exist, download and unzip them
+## sTEP 0: If the data files don't already exist, download and unzip them
 if(!dir.exists("UCI HAR Dataset")) {
   download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "./data.zip")
   unzip("data.zip")
 }
 
-## read the data from the downloaded files
+# read the data from the downloaded files
 activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt", as.is = TRUE)
 featureLabels <- read.table("UCI HAR Dataset/features.txt", as.is = TRUE)
 trainX <- read.table("UCI HAR Dataset/train/X_train.txt")
@@ -14,10 +14,27 @@ testX <- read.table("UCI HAR Dataset/test/X_test.txt")
 testY <- read.table("UCI HAR Dataset/test/Y_test.txt")
 testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
 
-## combine the training and test data sets
-## then remove the individual sets from memory
+# combine the training and test data sets
 dataX <- rbind(trainX, testX)
 dataY <- rbind(trainY, testY)
 dataSubjects <- rbind(trainSubjects, testSubjects)
-rm(trainX, trainY, testX, testY, trainSubjects, testSubjects)
+
+# apply names to the columbs of the X, Y, and Subject sets
+names(dataX) <- featureLabels[,2]
+names(dataY) <- "Activity ID"
+names(dataSubjects) <- "Subject"
+
+#extract the "mean" or "std" measures from the X data set then combine Subjects, Y, and X into one set
+dataX <- dataX[,grep("mean|std", names(dataX))]
+data <- cbind(dataSubjects, dataY, dataX)
+
+# remove the unneeded intermediate tables/frames from memory
+rm(trainX, trainY, testX, testY, trainSubjects, testSubjects, dataX, dataY, dataSubjects, featureLabels)
+
+# Merge the activity labels
+data <- merge(activityLabels, data, by.x = 1, by.y = "Activity ID")[-1]
+names(data)[1] <- "Activity"
+
+
+
 
